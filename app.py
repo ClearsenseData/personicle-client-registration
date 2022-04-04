1662eac80875f09e26e187e5e93ab4cf79e9c817
tirth-clearsense <tpatel@clearsense.com>
2022-04-04 14:36:19 -0500

    Allow scopes update

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from crypt import methods
+from os import access
 from sys import api_version
 from flask import Flask, render_template, url_for, flash, redirect,request, logging
 import requests
 from header import headers
 from config import client_registration,group_assignment_id, group_assignment_endpoint, policy_endpoint
-from forms import NewSecretDeleteGetForm, RegisterForm, UpdateForm, GetScopesForm
+from forms import NewSecretDeleteGetForm, RegisterForm, UpdateForm, GetScopesForm, UpdateScopesForm
 app = Flask(__name__)
 app.secret_key="secret_key"
 
@@ -56,9 +57,7 @@ def create_policy(res,headers):
     # print(created_policy.status_code)
 
     return created_policy
-
-def create_rule(res,headers,access_scopes,created_policy):
-    access_scopes += ['openid','email','profile','phone','address','offline_access']
+def get_rule_data(access_scopes):
     rule_data = {
         "type": "RESOURCE_ACCESS",
         "name": "Data access policy Rule",
@@ -89,22 +88,10 @@ def create_rule(res,headers,access_scopes,created_policy):
                 "refreshTokenWindowMinutes": 10080,
             },
         },
-    }   
-    created_rule= requests.post(f"{policy_endpoint}/{created_policy.json()['id']}/rules",json=rule_data,headers=headers)
-    # print(created_rule)
-    # print(created_rule.status_code)
-    return created_rule
-
-@app.route('/register', methods=['GET','POST'])
-def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        client_name = form.client_name.data
-        client_uri = form.client_uri.data
-        redirect_uri = form.redirect_uri.data
-        access_scopes = form.access_scopes.data
-        # print(access_scopes)
-        json_data = {
+    }  
+    return rule_data
+def get_app_data(client_name,client_uri,redirect_uri):
+    app_data = {
         'client_name': client_name,
         'client_uri': client_uri,
         'application_type': 'web',
@@ -127,8 +114,26 @@ def register():
         'token_endpoint_auth_method': 'client_secret_basic',
         # 'initiate_login_uri': 'https://www.example-application.com/oauth2/login',
     }
+    return app_data
 
-        res = requests.post(f"{client_registration}", json=json_data, headers=headers)
+def create_rule(res,headers,access_scopes,created_policy):
+    access_scopes += ['openid','email','profile','phone','address','offline_access']
+    rule_data = get_rule_data(access_scopes)
+    created_rule= requests.post(f"{policy_endpoint}/{created_policy.json()['id']}/rules",json=rule_data,headers=headers)
+    return created_rule
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        client_name = form.client_name.data
+        client_uri = form.client_uri.data
+        redirect_uri = form.redirect_uri.data
+        access_scopes = form.access_scopes.data
+        # print(access_scopes)
+        app_data = get_app_data(client_name,client_uri,redirect_uri)
+        
+        res = requests.post(f"{client_registration}", json=app_data, headers=headers)
         # print(res.json())
         if res.status_code == 201:
             assign_users = requests.put(f"{group_assignment_endpoint}/{res.json()['client_id']}/groups/{group_assignment_id}", headers=headers)
@@ -158,33 +163,10 @@ def update_app():
         client_name = form.client_name.data
         client_uri = form.client_uri.data
         redirect_uri = form.redirect_uri.data
+        app_data = get_app_data(client_name,client_uri,redirect_uri)
+        app_data['client_id'] = client_id
 
-        json_data = {
-            'client_id': client_id,
-            'client_name': client_name,
-            'client_uri': client_uri,
-            'application_type': 'web',
-            'redirect_uris': [
-                redirect_uri,
-            ],
-            'post_logout_redirect_uris': [
-                'http://localhost:3000/oauth/callback/postLogoutRedirectUri',
-            ],
-            'response_types': [
-                'code',
-                'id_token',
-                'token',
-            ],
-            'grant_types': [
-                'authorization_code',
-                'refresh_token',
-                'implicit'
-            ],
-            'token_endpoint_auth_method': 'client_secret_basic',
-            # 'initiate_login_uri': 'https://www.example-application.com/oauth2/login',
-        }
-
-        res = requests.put(f"{client_registration}/{client_id}", json=json_data, headers=headers)
+        res = requests.put(f"{client_registration}/{client_id}", json=app_data, headers=headers)
         # print(res.json())
         if res.status_code == 200:
             flash("Your app settings are successfully updated",'success')
@@ -200,11 +182,9 @@ def new_secret():
 
     if request.method == 'POST' and form.validate():
         client_id = form.client_id.data
-
         res = requests.post(f"{client_registration}/{client_id}/lifecycle/newSecret", headers=headers)
 
         if res.status_code == 200:
-            # print(res.json())
             flash(f"Your new client secret is {res.json()['client_secret']}", 'success')
         else :
             flash(f"Something went wrong. Unable to generate new secret", 'warning')
@@ -233,7 +213,6 @@ def delete_app():
     
     if request.method == 'POST' and form.validate():
         client_id = form.client_id.data
-
         res = requests.delete(f"{client_registration}/{client_id}", headers=headers)
         
         if res.status_code == 204:
@@ -247,11 +226,11 @@ def delete_app():
 @app.route('/get-scopes', methods=['GET','POST'])
 def get_scopes():
     form = GetScopesForm(request.form)
-
+ 
     if request.method == 'POST' and form.validate():
         client_id = form.client_id.data.strip()
-
         policies = requests.get(f"{policy_endpoint}",headers=headers)
+
         for policy in policies.json():
             if client_id in policy['conditions']['clients']['include']:
                 rules = requests.get(f"{policy['_links']['rules']['href']}", headers=headers)
@@ -261,6 +240,29 @@ def get_scopes():
                     return redirect(url_for('index'))
         flash('No scopes found for your client id', 'warning')
     return render_template('getScopes.html', form=form) 
+
+@app.route('/update-scopes', methods=['GET','POST'])
+def update_scopes():
+    form = UpdateScopesForm(request.form)
+ 
+    if request.method == 'POST' and form.validate():
+        client_id = form.client_id.data.strip()
+        access_scopes = form.access_scopes.data
+        access_scopes += ['openid','email','profile','phone','address','offline_access']
+        policies = requests.get(f"{policy_endpoint}",headers=headers)
+        for policy in policies.json():
+            if client_id in policy['conditions']['clients']['include']:
+                rules = requests.get(f"{policy['_links']['rules']['href']}", headers=headers)
+                rule_url = rules.json()[0]['_links']['self']['href']
+                rule_data = get_rule_data(access_scopes)
+                update_rule = requests.put(rule_url, json=rule_data, headers=headers)
+                print(update_rule.status_code)
+                if rules.status_code == 200:
+                #     scopes = rules.json()[0]['conditions']['scopes']['include']
+                    flash(f'Scopes updated successfully', 'success')
+                    return redirect(url_for('index'))
+        flash('No scopes found for your client id', 'warning')
+    return render_template('updateScopes.html', form=form) 
 
 if __name__ == '__main__':
     
--- forms.py
+++ forms.py
@@ -5,19 +5,7 @@ from flask import Flask, render_template, url_for, flash, redirect,request, logg
 from wtforms import Form, StringField, validators, SelectMultipleField, TextAreaField
 from header import headers
 
-
-class NewSecretDeleteGetForm(Form):
-    client_id = StringField("Enter your client id",[validators.DataRequired()])
-
-class GetScopesForm(Form):
-    client_id = StringField("Enter your client id",[validators.DataRequired()])
-
-class RegisterForm(Form):
-    client_name = StringField('Your application name', [validators.Length(min=5,max=50), validators.DataRequired()])
-    client_uri = StringField('Your application URL', [ validators.DataRequired()])
-    redirect_uri = StringField('Your redirect URI', [ validators.DataRequired()])
-    access_scopes = SelectMultipleField('What user data would you like to access? (Ctrl + click to select multiple fields)', [ validators.DataRequired()],
-     choices=[('com.personicle.individual.datastreams.heartrate', 'heartrate read'),
+scope_choices = [('com.personicle.individual.datastreams.heartrate', 'heartrate read'),
     #  ('com.personicle.individual.datastreams.heartrate.write', 'heartrate write') ,
      ('com.personicle.individual.datastreams.heart_intensity_minutes', 'heart intensity minutes read'),
     #  ('com.personicle.individual.datastreams.heart_intensity_minutes.write', 'heart intensity minutes write'),
@@ -40,9 +28,25 @@ class RegisterForm(Form):
     ('com.personicle.individual.datastreams.blood_pressure.systolic', 'blood pressure systolic read'),
     ('com.personicle.individual.datastreams.blood_pressure.diastolic', 'blood pressure diastolic read'),
     ('com.personicle.individual.datastreams.body_temperature', 'body temperature read')
+     ]
+class NewSecretDeleteGetForm(Form):
+    client_id = StringField("Enter your client id",[validators.DataRequired()])
 
-     
-     ])
+class GetScopesForm(Form):
+    client_id = StringField("Enter your client id",[validators.DataRequired()])
+  
+
+class UpdateScopesForm(Form):
+    client_id = StringField("Enter your client id",[validators.DataRequired()])
+    access_scopes = SelectMultipleField('Update your scopes (Ctrl + click to select multiple fields)', [ validators.DataRequired()],
+     choices=scope_choices)
+
+class RegisterForm(Form):
+    client_name = StringField('Your application name', [validators.Length(min=5,max=50), validators.DataRequired()])
+    client_uri = StringField('Your application URL', [ validators.DataRequired()])
+    redirect_uri = StringField('Your redirect URI', [ validators.DataRequired()])
+    access_scopes = SelectMultipleField('What user data would you like to access? (Ctrl + click to select multiple fields)', [ validators.DataRequired()],
+     choices=scope_choices)
 
 class UpdateForm(Form):
     client_id = StringField("Enter you client id",[validators.DataRequired()])
